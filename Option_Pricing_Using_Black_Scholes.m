3905ac068669407cb15bdbd623197756f58ef88e
Narayan Jee Jha <58243776+9mpd@users.noreply.github.com>
2022-04-18 21:32:09 +0530

    Update Option_Pricing_Using_Black_Scholes.m

--- Option_Pricing_Using_Black_Scholes.m
+++ Option_Pricing_Using_Black_Scholes.m
@@ -1,3 +1,9 @@
+Copyright (c) 2022 Narayan Jee Jha
+All rights reserved.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree
+
 % European Option Pricing Using the Black Scholes Formula.
 
 clear    % Clear MATLAB Workspace.
